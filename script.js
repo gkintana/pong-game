12466de3323c885d1004e91ce9a08bfbfd99d1f9
gkintana <81775867+gkintana@users.noreply.github.com>
2023-02-25 21:39:30 +0400

    collision update & added key event for start message

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 import Ball from "./Ball.js"
 import Paddle from "./Paddle.js"
 import { getVhProperty } from "./utils.js"
+import { getVhUnits } from "./utils.js"
 
 const ball = new Ball(document.getElementById("ball"))
 const leftPaddle = new Paddle(document.getElementById("player1-paddle"))
 const leftScore = document.getElementById("player1-score")
 const rightPaddle = new Paddle(document.getElementById("player2-paddle"))
 const rightScore = document.getElementById("player2-score")
+const winningPoint = 2
 
 let previousTime
 
-function updateFrame(currentTime) {
+function gameLoop(currentTime) {
+	updateStartScreen("none")
+	document.removeEventListener("keydown", waitForKeyPress)
+
 	if (previousTime != null) {
 		const diff = currentTime - previousTime
 		ball.update(diff, [leftPaddle.rect(), rightPaddle.rect()])
@@ -19,11 +24,26 @@ function updateFrame(currentTime) {
 
 	if (isRallyFinished()) {
 		addPointToWinner()
-		ball.reset()
+		resetGameObjects()
+
+		if (getScore(leftScore) >= winningPoint || getScore(rightScore) >= winningPoint) {
+			updateStartScreen("flex")
+			resetScore()
+			previousTime = null
+			document.addEventListener("keydown", waitForKeyPress)
+			return
+		} else {
+			ball.reset()
+		}
 	}
 
 	previousTime = currentTime
-	window.requestAnimationFrame(updateFrame)
+	window.requestAnimationFrame(gameLoop)
+}
+
+function updateStartScreen(value) {
+	var startScreen = document.getElementById("start-screen")
+	startScreen.style.display = value
 }
 
 function isRallyFinished() {
@@ -40,13 +60,35 @@ function addPointToWinner() {
 	rect.right >= window.innerWidth ? increase(leftScore) : increase(rightScore)
 }
 
+function resetGameObjects() {
+	ball.reset()
+	leftPaddle.reset()
+	rightPaddle.reset()
+}
+
+function resetScore() {
+	leftScore.textContent = 0
+	rightScore.textContent = 0
+}
+
+function getScore(elementId) {
+	return parseInt(elementId.textContent)
+}
+
+function waitForKeyPress(event) {
+	if (event.keyCode === 13) {
+		gameLoop()
+	}
+}
+
+document.addEventListener("keydown", waitForKeyPress)
+
 document.addEventListener("mousemove", element => {
 	leftPaddle.position = (element.y / window.innerHeight) * 100
+
 	if (leftPaddle.rect().top <= getVhProperty("score")) {
-		leftPaddle.position = (getVhProperty("score") / window.innerHeight * 100) + ((getVhProperty("player1-paddle") / 2) / window.innerHeight * 100)
+		leftPaddle.position = getVhUnits("score") + (getVhUnits("player1-paddle") / 2)
 	} else if (leftPaddle.rect().bottom >= window.innerHeight) {
-		leftPaddle.position = 100 - ((getVhProperty("player1-paddle") / 2) / window.innerHeight * 100)
+		leftPaddle.position = 100 - (getVhUnits("player1-paddle") / 2)
 	}
 })
-
-window.requestAnimationFrame(updateFrame)
--- utils.js
+++ utils.js
@@ -3,3 +3,7 @@ export function getVhProperty(elementId) {
 	const objectStyle = window.getComputedStyle(object)
 	return parseFloat(objectStyle.getPropertyValue("height"))
 }
+
+export function getVhUnits(elementId) {
+	return getVhProperty(elementId) / window.innerHeight * 100
+}
